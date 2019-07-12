f58a2aaaf1178de84c78afb0659aba2f94da9eb5
Alex Chomiak <alex.chomiak@gmail.com>
2019-07-12 14:09:18 -0500

    created workflow

---
+++ librarysettings.js
@@ -0,0 +1,6 @@
+module.exports =  {
+    fileName: "LibraryFile.js",
+    libraryName: "Library",
+    libraryTarget: "var",
+    libraryBuildFolderName: "LibraryBuild"
+}
---
+++ src/lib.js
@@ -0,0 +1,4 @@
+// import all required modules here for library
+
+
+// export all modules to be exposed in library
